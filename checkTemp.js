6af9de1bf89b096a4acb3d42fde2cd986314e56a
Sven Laht <104273841+HappySven06@users.noreply.github.com>
2022-09-16 09:14:05 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,17 @@
+function checkTemp()
+{
+    var temp;
+
+    if (temp < 21)
+    {
+        Ans = -1
+    }
+    else if (temp > 40)
+    {
+        Ans = 1
+    }
+    else if (temp != 21 && 40)
+    {
+        Ans = 0
+    }
+}
