55c7ed07ab2ac5644efee0b0e363d0cca7b712da
Sven Laht <104273841+HappySven06@users.noreply.github.com>
2022-10-07 11:19:43 +0300

    Update orderSummary.js

--- orderSummary.js
+++ orderSummary.js
@@ -5,8 +5,8 @@ function orderSummary(tellimus)
     for (var i = 0, row; row = tellimus.rows[i]; i++) {
         console.log(row.name)
         console.log(row.amount)
-        vatAmount = row.price * row.vat;
-        vatPrice = row.price + vatAmount * row.amount;
+        vatAmount = row.price * row.vat * row.amount;
+        vatPrice = row.price + vatAmount;
         console.log(vatPrice)
         Total += vatPrice;
     }
